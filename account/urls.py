160521f39886cc1b636635b800c09ed2891e5a2e
Alex Zhang <me@zhangzhixiong.com>
2020-03-18 21:55:52 +0800

    using django auth system

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-        path('',views.user_login,name="user_login"),
+        # path('login',views.user_login,name="user_login"),
+        path('login',auth_views.LoginView.as_view(),name="user_login"),
         ]
