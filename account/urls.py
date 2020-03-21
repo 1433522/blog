1e69809dd533945382e6f0a40d7b50de7dafdf4d
Alex Zhang <me@zhangzhixiong.com>
2020-03-21 13:35:10 +0800

    add logout functionality

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
         # path('login',views.user_login,name="user_login"),
         path('login',auth_views.LoginView.as_view(),name="user_login"),
+        path('logout',auth_views.LogoutView.as_view(template_name='account/logout.html'),name="user_logout"),
         ]
